8310f1d0fcf97327d819680efaeb19e93666615b
Kasidis <pklpop@hotmail.com>
2015-08-30 20:07:15 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixter1::Application.config.secret_key_base = '413eb1c5e8dc2b641551973aaeaf1f94b5fc9d0f604574a112150aebab4df61dd8ad1b33a8c6201bc5c4c4dc31a32e32f82b7c87adda945bea55e605d27027b3'
